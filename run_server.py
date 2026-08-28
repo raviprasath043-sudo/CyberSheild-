15f155e8fe65fda957d2a7ca917722bd80df587d
raviprasath <raviprasath043@gmail.com>
2026-08-28 20:32:50 +0530

    Configure production server for deployment

--- run_server.py
+++ run_server.py
@@ -2,18 +2,19 @@ import os
 from waitress import serve
 from app import app
 
-PORT = int(os.environ.get("PORT", 5000))
-
 print("====================================")
 print(" 🛡️ CyberShield Production Server")
 print("====================================")
-print("Server running at:")
-print(f"http://0.0.0.0:{PORT}")
+
+port = int(os.environ.get("PORT", 5000))
+
+print("Server running on:")
+print(f"http://0.0.0.0:{port}")
 print("Press CTRL+C to stop.")
 print()
 
 serve(
     app,
     host="0.0.0.0",
-    port=PORT
+    port=port
 )
